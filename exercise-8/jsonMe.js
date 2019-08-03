3d1049614a27c22db2d1d99fdde587d38dbca02e
Chris Holland <christopherholland@uvic.ca>
2019-08-02 21:20:50 -0700

    Exercise 8 Complete, last challenge

---
+++ exercise-8/jsonMe.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = process.argv[2]
+const file = process.argv[3]
+
+app.get('/books', function(req, res) {
+    fs.readFile(file, function(err, data) {
+        const object = JSON.parse(data)
+        res.json(object)
+    })
+})
+
+app.listen(port)
